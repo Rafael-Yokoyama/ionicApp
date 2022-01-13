f06868065f27b58c9ea82de493cd41c0ba2be639
Rafael Yokoyama <rafaelyokoyama925@gmail.com>
2022-01-13 14:19:28 -0300

    create interface pokemons

---
+++ src/app/interfaces/pokemons.ts
@@ -0,0 +1,10 @@
+export interface PokemoInterface {
+  id: number;
+  name: string;
+  num: string;
+  img: string;
+  type: Array<string>;
+  weight: string;
+  candy: string;
+  weaknesses: Array<string>;
+}
